c95d3d62c26f6990ff586ccbe609455b760e6e85
Sebastien Leclerc Lavallee <seb.leclerc@me.com>
2023-08-22 22:41:59 -0400

    Add all other players (skater) or current pool roster

--- Hockey.rb
+++ Hockey.rb
@@ -17,9 +17,26 @@ class Hockey < Thor
         @dataService.updateTeams
 
         rosterPlayerIds = [
-            8478402 # Connor McDavid
+            8478402, # Connor McDavid
+            8473419, # Brad Marchand
+            8474564, # Steven Stamkos
+            8479343, # Clayton Keller
+            8476539, # Jonathan Marchesseault
+            8478864, # Kirill Kaprizov
+            8484144, # Connor Bedard
+            8482117, # Lukas Reichel
+            8477451, # Ryan Hartman
+            8481093, # RHP
+            8482745, # Mason McTavish
+            8483524, # Share Wrigth
+            8479410, # Sergachev
+            8474590, # John Carlson
+            8482803, # Olen Zellweger
+            8482730, # Brandt Clark
+            8483460, # David Jiricek
+            8483495, # Simon Nemec
         ]
-        
+
         @dataService.updatePlayers(rosterPlayerIds)
         @dataService.updatePlayerArchiveStats(rosterPlayerIds)
     end
