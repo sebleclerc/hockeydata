2f3df2cc523340d93389cfed390c0e27d15d32c2
Sebastien Leclerc Lavallee <seb.leclerc@me.com>
2023-11-03 16:33:31 -0400

    Convert player task to BaseCommand

---
+++ commands/PlayerCommand.rb
@@ -0,0 +1,30 @@
+#
+# PlayerCommand
+#
+
+class PlayerCommand < BaseCommand
+  desc "player info playerId", "Show informations about a specific player."
+    def info(playerId)
+      initTask()
+
+      Logger.taskTitle "Player for ID #{playerId}"
+
+      player = @dbService.getPlayerForId(playerId)
+
+      Logger.info "Name: #{player.fullName} (#{player.primaryNumber})"
+      Logger.info "Birth #{player.birthDate} in #{player.birthLocation}"
+      Logger.info ""
+      Logger.info PlayerSeasonStats.formattedHeaderString()
+      Logger.info ""
+
+      stats = @dbService.getPlayerSeasonStatsForPlayerIdAndSeason(playerId)
+
+      stats.each do |stat|
+        Logger.info stat.formattedString(player.positionCode)
+      end
+
+      Logger.taskEnd()
+    end
+
+    default_task :info
+end
--- hockey.rb
+++ hockey.rb
@@ -9,7 +9,7 @@ Dir["./helpers/*.rb"].each {|file| require file }
 Dir["./services/*.rb"].each {|file| require file }
 
 class Hockey < Thor
-    desc "hockey cache", "Fetch and cache files"
+    desc "cache", "Fetch and cache files"
     subcommand "cache", CacheCommand
 
     desc "import", "Import JSON files into databse."
@@ -19,41 +19,13 @@ class Hockey < Thor
     subcommand "roster", RosterCommand
 
     desc "player playerId", "Show informations about a specific player."
-    def player(playerId)
-        dbService = DatabaseService.new
+    subcommand "player", PlayerCommand
 
-        Logger.taskTitle "Player for ID #{playerId}"
-
-        player = dbService.getPlayerForId(playerId)
-
-        Logger.info "Name: #{player.fullName} (#{player.primaryNumber})"
-        Logger.info "Birth #{player.birthDate} in #{player.birthLocation}"
-        Logger.info ""
-        Logger.info PlayerSeasonStats.formattedHeaderString()
-        Logger.info ""
-
-        stats = dbService.getPlayerSeasonStatsForPlayerIdAndSeason(playerId)
-
-        stats.each do |stat|
-            Logger.info stat.formattedString(player.positionCode)
-        end
-
-        Logger.taskEnd()
-    end
-
-    desc "hockey pool", "All pool related tasks."
+    desc "pool", "All pool related tasks."
     subcommand "pool", PoolCommand
 
-    desc "hockey salary", "All salary related tasks."
+    desc "salary", "All salary related tasks."
     subcommand "salary", SalaryCommand
-
-    no_tasks do
-        def initTask()
-            @dbService = DatabaseService.new
-            @cacheService = CacheService.new
-            @importService = ImportService.new(@dbService)
-        end
-    end
 end
 
 Hockey.start(ARGV)
